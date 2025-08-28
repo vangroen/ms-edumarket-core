4204380af0d0452eb0e57436756f05be462565b2
Vangroen <petzdev@gmail.com>
2025-08-28 00:12:20 -0500

    fix: se corrige guardado de data de cronograma de pagos

--- src/main/java/com/ctcse/ms/edumarket/core/paymentSchedule/service/PaymentScheduleService.java
+++ src/main/java/com/ctcse/ms/edumarket/core/paymentSchedule/service/PaymentScheduleService.java
@@ -73,6 +73,6 @@ public class PaymentScheduleService {
 
         PaymentScheduleEntity savedPaymentScheduleEntity = paymentScheduleRepository.save(paymentScheduleEntity);
 
-        return convertToDto(paymentScheduleEntity);
+        return convertToDto(savedPaymentScheduleEntity);
     }
 }
